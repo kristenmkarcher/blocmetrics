05db5d0913fb1a418b5c4b7d6cea8be7367c7d45
KristenKarcher <kristenmkarcher@gmail.com>
2016-10-24 22:39:28 -0400

    Register an Application complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017161915) do
+ActiveRecord::Schema.define(version: 20161025013035) do
+
+  create_table "registered_applications", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/factories/registered_applications.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :registered_application do
+    user "MyString"
+  end
+end
