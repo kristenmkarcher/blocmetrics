536d7383ad571870cebe0ea047388647d00b88f5
KristenKarcher <kristenmkarcher@gmail.com>
2016-10-25 20:59:13 -0400

    Register an Application updated

--- app/controllers/registered_applications_controller.rb
+++ app/controllers/registered_applications_controller.rb
@@ -11,52 +11,53 @@ class RegisteredApplicationsController < ApplicationController
 
   def new
     @registered_application = RegisteredApplication.new
-end
+  end
 
   def edit
     @registered_application = RegisteredApplication.find(params[:id])
   end
 
   def create
-   @registered_application = RegisteredApplication.new
-   @registered_application.assign_attributes(registered_application_params)
+    @registered_application = RegisteredApplication.new
+    @registered_application.assign_attributes(registered_application_params)
 
-   if @registered_application.save
-     flash[:notice] = "The #{@registered_application.name} was saved successfully."
-     redirect_to @registered_application
-   else
-     flash[:error] = "There was an error saving the Registered Application."
-     render :new
-   end
- end
-
-  def update
-     @registered_application = RegisteredApplication.find(params[:id])
-     @registered_application.assign_attributes(registered_application_params)
-
-     if @registered_application.save
-       flash[:notice] = "#{@registered_application.name} was successfully updated."
-       redirect_to @registered_application
-     else
-       flash[:alert] = "There was an error saving the Registered Application. Please try again."
-       render :edit
-     end
-   end
-
-   def destroy
-     	@registered_application = RegisteredApplication.find(params[:id])
-
-     	if @registered_application.destroy
-     		flash[:notice] = "\"#{@registered_application.name}\" was deleted successfully."
-     		redirect_to registered_applications_path
-     	else
-     		flash[:alert] = "There was an error deleting the Registered Application."
-     		render :show
-     	end
-     end
-
-    private
-    def registered_application_params
-      params.require(:registered_application).permit(:name, :url)
+    if @registered_application.save
+      flash[:notice] = "The #{@registered_application.name} was saved successfully."
+      redirect_to @registered_application
+    else
+      flash[:error] = "There was an error saving the Registered Application."
+      render :new
     end
   end
+
+  def update
+    @registered_application = RegisteredApplication.find(params[:id])
+    @registered_application.assign_attributes(registered_application_params)
+
+    if @registered_application.save
+      flash[:notice] = "#{@registered_application.name} was successfully updated."
+      redirect_to @registered_application
+    else
+      flash[:alert] = "There was an error saving the Registered Application. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @registered_application = RegisteredApplication.find(params[:id])
+
+    if @registered_application.destroy
+      flash[:notice] = "\"#{@registered_application.name}\" was deleted successfully."
+      redirect_to registered_applications_path
+    else
+      flash[:alert] = "There was an error deleting the Registered Application."
+      render :show
+    end
+  end
+
+  private
+  def registered_application_params
+    params.require(:registered_application).permit(:name, :url)
+  end
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
-
-  has_many :registered_applications, dependent: :destroy
 end
